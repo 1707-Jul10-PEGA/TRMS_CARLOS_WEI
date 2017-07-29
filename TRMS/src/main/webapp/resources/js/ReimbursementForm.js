fec6f2f49445db3282646ccb6fc9b064921a9a9d
Carlos Gastelum <carlosgstlm@gmail.com>
2017-07-29 15:09:51 -0400

    pom issue

--- TRMS/src/main/webapp/resources/js/ReimbursementForm.js
+++ TRMS/src/main/webapp/resources/js/ReimbursementForm.js
@@ -4,18 +4,20 @@ window.onload = function() {
 
 function handleEvent(){
     $.post("ReimbursementForm.html",makeForm(),function(response,status){
-        console.log("")
+        console.log(response);
+        console.log(status);
     });
 }
 
 function makeForm(){
-    var Form = {};
+    var form = {};
     form.date = document.getElementById("date").value;
     form.location = document.getElementById("location").value;
     form.cost = document.getElementById("cost").value;
     form.timeStamp = document.getElementById("time").value;
     form.description = document.getElementById("description").value;
     form.gradingformat = document.getElementById("gradingformat").value;
+    console.log(document.getElementById("justification"));
     form.justification = document.getElementById("justification").value;
     form.eventtype = document.getElementById("eventtype").value;
 }
